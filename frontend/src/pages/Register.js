17c85d1f5f4f5471a7a08857f457bfc9ac3665a9
Felix Sihitshuwam <felixsigit@gmail.com>
2024-04-16 11:51:54 +0100

    Worked on registration page.

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -21,69 +21,77 @@ function Register() {
       <div className="section-1">
         <div className="bg-white">
           <img src={logo} alt="" />
-          <div className="person">
-            <h1>Personal Information</h1>
-            <br />
-            <label htmlFor="estateNameOrIndividual">
-              Estate Name Or Individual*
-            </label>
-            <input type="text" id="estateNameOrIndividual" required placeholder="John Doe" />
-
-            <label htmlFor="service">
-              Service / Flat Apartment*
-            </label>
-            <input type="text" id="service" required placeholder="" />
-
-            <label htmlFor="flatOccupant">
-              Name of Flat Occupant*
-            </label>
-            <input type="text" id="flatOccupant" required placeholder="" />
-
-            <label htmlFor="serviceContract">Service Contract</label>
-            <br />
-            <select id="serviceContract">
-              <option value="priorityService">Priority Service</option>
-              <option value="priorityPlusService">Priority Plus Service</option>
-            </select>
-
-            <label htmlFor="location">
-              Location*
-            </label>
-            <input type="text" id="location" required placeholder="" />
-
-            <label htmlFor="email">
-                      Email Address*
-            </label>
-            <input type="email" required placeholder='johndoe123@gmail.com' />
-
-            <label htmlFor="password">
-              Password*
-            </label>
-            <input type="password" id="password" required placeholder="******" />
-
-            <div>
-              <label htmlFor="Image">Uplaod Picture or Estate Logo</label>
-              <form onSubmit={handleSubmit}>
-                <input
-                  type="file"
-                  id="imageInput"
-                  accept="image/*"
-                  onChange={handleImageChange}
-                />
-              </form>
-              {/* Display the selected image */}
+          <div className="my-3">
+            <h3 className="text-center my-3">Account Type</h3>
+            <div className="row gap-0">
+              <div className="col-6 p-0 pe-1">
+                <div className="d-grid">
+                  <button className="btn btn-outline-secondary" type="button" disabled>Estate</button>
+                </div>
+              </div>
+              <div className="col-6 p-0 ps-1">
+                <div className="d-grid gap-0">
+                  <button className="btn btn-primary" type="button">Individual</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="mt-5">
+            <h3 className="text-center">Personal Information</h3>
+            <form onSubmit={handleSubmit}>
+              <div className="my-5">
+                <div className="col-12">
+                  <label className="form-label">Email</label>
+                  <input type="email" className="form-control" name="email" />
+                </div>
+                <div className="col-12">
+                  <label className="form-label">Password</label>
+                  <input type="password" className="form-control" name="password" />
+                </div>
+                <div className="col-12">
+                  <label className="form-label">Full Name</label>
+                  <input type="text" className="form-control" name="name" />
+                </div>
+                <div className="col-12">
+                  <label className="form-label">Apartment</label>
+                  <input type="text" className="form-control" name="apartment" />
+                </div>
+                <div className="col-12">
+                  <label className="form-label">Address</label>
+                  <input type="text" className="form-control" name="address" />
+                </div>
+                <div className="col-12">
+                  <label className="form-label">Service Type</label>
+                  <select className="form-select" aria-label="Default select example" name="service">
+                    <option selected>Select your Service Type</option>
+                    <option value="1">Priority</option>
+                    <option value="2">Priority Plus</option>
+                  </select>
+                </div>
+                <div className="col-12">
+                  <label className="form-label">House Photo</label>
+                  <div>
               {selectedImage && (
-                <div>
-                  <h3>Selected Image Preview:</h3>
+                <div className="text-center my-3">
                   <img src={URL.createObjectURL(selectedImage)} alt="Selected" />
                 </div>
               )}
             </div>
+                  <input
+                    type="file" id="imageInput" className="form-control"
+                    accept="image/*"
+                    onChange={handleImageChange}
+                  />
+                </div>
+                <div className="col-8 offset-2">
+                  <div className="d-grid">
+                    <button className="btn-primary btn" type="submit">Sign Me</button>
+                  </div>
+                </div>
+              </div>
+            </form>
           </div>
-          
-          <div className="btn-2">
-            <button>Sign me up</button>
-          </div>
+
         </div>
       </div>
     </div>
