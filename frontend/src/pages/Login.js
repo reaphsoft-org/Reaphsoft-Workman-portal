757fbf7360bf8139eb9a17b59e9c0807f51a5a24
Felix Sihitshuwam <felixsigit@gmail.com>
2024-04-16 20:07:30 +0100

    Updated posting codes for login.

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,15 +1,43 @@
 import '../App.css';
-import { FaFacebookF } from "react-icons/fa6";
-import { FiMail } from 'react-icons/fi'; // Import the FiMail component for Gmail
-import { FiLinkedin } from 'react-icons/fi';
-import { FiInstagram } from 'react-icons/fi'; // Import the FiInstagram component for Instagram
-import { FiX } from 'react-icons/fi';
 import { IoLockOpen } from "react-icons/io5";
 import { Link } from 'react-router-dom';
-import logo from './logo.jpeg';
+import {useState} from "react";
 
 
 function Login() {
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
+  const handleInputChange = (e) => {
+    setData({...data, [e.target.name]: e.target.value});
+  }
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = JSON.stringify(data);
+    try {
+      const response = await fetch('http://localhost:3001/auth/login', {
+        method: 'POST',
+        body: formData,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      if (!response.ok){
+        // show toast
+        console.log("error");
+        return;
+      }
+
+      const data = await response.json();
+      // Handle the response data
+      if (data.status === true) {
+        window.location.href = "/Login";
+      }
+    }catch (e) {
+      console.error('Error posting form data:', e);
+    }
+  }
   return (
     <div className="App">
       <div className="hero_area yes" style={{ width: '100%', height: '100vh' }}>
@@ -24,20 +52,21 @@ function Login() {
                   <p>Welcome to the Reaphsoft Workman Portal Login</p>
                   
                 </div>
-
                 <div className="main-2">
-
                   <div className="bg mb-5">
-                    <form>
+                    <form onSubmit={handleSubmit}>
                       <h5 className="mb-4">Please log in if you have an account with us</h5>
                       <label htmlFor="email" className="form-label">Email Address</label>
-                      <input type="email" required placeholder='johndoe123@gmail.com' className="form-control" autoComplete="email"/>
+                      <input type="email" required placeholder='johndoe123@gmail.com' className="form-control"
+                             autoComplete="email" name="email" value={data.email} onChange={handleInputChange}/>
                       <label htmlFor="password" className="form-label">Password</label>
-                      <input type="password" required placeholder='*********' className="form-control" autoComplete="current-password"/>
+                      <input type="password" required placeholder='*********' className="form-control"
+                             autoComplete="current-password" name="password" value={data.password}
+                             onChange={handleInputChange} />
                       <div className="row">
                         <div className="col-6">
                           <div className="d-grid">
-                            <button className="btn btn-primary">Login</button>
+                            <button className="btn btn-primary" type="submit">Login</button>
                           </div>
                         </div>
                         <div className="col-6">
