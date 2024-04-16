502e62a9108fa6c6d320b32056a124484066c688
Felix Sihitshuwam <felixsigit@gmail.com>
2024-04-16 13:02:36 +0100

    Created user entity.

---
+++ backend/src/entity/user.ts
@@ -0,0 +1,32 @@
+// user.entity.ts
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'integer' })
+  accountType: number;
+
+  @Column({ type: 'varchar', length: 100 })
+  email: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  password: string;
+
+  @Column({ type: 'varchar', length: 40 })
+  fullname: string;
+
+  @Column({ type: 'varchar', length: 20 })
+  apartment: string;
+
+  @Column({ type: 'varchar', length: 50 })
+  address: string;
+
+  @Column({ type: 'integer' })
+  serviceType: number;
+
+  @Column({ type: 'varchar', length: 50 })
+  photoURL: string;
+}
