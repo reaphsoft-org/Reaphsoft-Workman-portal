987f723237d7b097de0a7ed68c29a1f9d3bc94ee
Felix Sihitshuwam <felixsigit@gmail.com>
2024-04-16 16:08:13 +0100

    Saves data in database

--- backend/src/accounts/accounts.service.ts
+++ backend/src/accounts/accounts.service.ts
@@ -1,11 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { CreateAccountDto } from './dto/create-account.dto';
+import { AppDataSource } from '../data-source';
+import { User } from '../entities/User';
 
 @Injectable()
 export class AccountsService {
-  async createAccount(createAccountDto: CreateAccountDto) {
+  async createAccount(createAccountDto: CreateAccountDto, file: any) {
     // Implement account creation logic here
-    console.log(createAccountDto);
-    return 'Account created successfully';
+    const accountRepository = AppDataSource.getRepository(User);
+    const user = new User();
+    user.accountType = createAccountDto.accountType;
+    user.email = createAccountDto.email;
+    user.password = createAccountDto.password;
+    user.fullname = createAccountDto.fullname;
+    user.apartment = createAccountDto.apartment;
+    user.address = createAccountDto.address;
+    user.serviceType = createAccountDto.serviceType;
+    user.photoURL = '';
+    // if (file != null) {
+    //   user.photoURL = file.name;
+    // }
+    await accountRepository.save(user);
+    return { resp: 'Account created successfully', status: true };
   }
 }
--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
+import { User } from './entities/User';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
@@ -10,7 +11,7 @@ export const AppDataSource = new DataSource({
   database: 'reaphsoft_workmen.db',
   synchronize: true,
   logging: false,
-  entities: ['src/entities/*.ts'],
+  entities: [User],
   migrations: ['src/migrations/*.ts'],
   subscribers: ['src/subscribers/*.ts'],
 });
