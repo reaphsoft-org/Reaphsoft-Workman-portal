b091bcc4d9fa8ada6fc60df622db8096cdee76a0
Felix Sihitshuwam <felixsigit@gmail.com>
2024-04-16 20:26:57 +0100

    Worked on authenticating a user.

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,8 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { AppDataSource } from '../data-source';
+import { User } from '../entities/User';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class AuthService {
+  private readonly userRepository: Repository<User> =
+    AppDataSource.getRepository(User);
   async validateUser(email: string, password: string) {
-    return true;
+    try {
+      const user = await this.findUserByEmail(email);
+      // Compare the password from the request with the password stored in the database
+      return user.password === password;
+    } catch (error) {
+      // Handle errors
+      return false;
+    }
+  }
+  async findUserByEmail(email: string): Promise<User> {
+    const user = await this.userRepository.findOne({ where: { email } });
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return user;
   }
 }
