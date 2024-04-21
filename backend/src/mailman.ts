1169bd0802de8b6a6a0ac3da5cf8acf1f2eece9a
Felix Sihitshuwam <felixsigit@gmail.com>
2024-04-21 13:49:35 +0100

    Worked on sending email with attachments.

--- backend/src/mailman.ts
+++ backend/src/mailman.ts
@@ -1,10 +1,12 @@
-import Mailgun from 'mailgun.js';
+import Mailgun, { MailgunMessageData } from 'mailgun.js';
 import { IMailgunClient } from 'mailgun.js/Interfaces';
+import * as fs from 'fs';
 
 export class Email {
   private mailgun: Mailgun;
   private mg: IMailgunClient;
   private domain: string;
+
   constructor() {
     this.mailgun = new Mailgun(FormData);
     this.mg = this.mailgun.client({
@@ -21,21 +23,49 @@ export class Email {
     text: string,
     html: string,
   ) {
+    const data = {
+      from: 'Reaphsoft Workmen <noreply@reaphsoft-workmen.com>',
+      to: [recipient],
+      subject: subject,
+      text: text,
+      html: html,
+    };
+    return await this.sendMail(data);
+  }
+
+  async sendTextMailWithAttachment(
+    recipient: string,
+    subject: string,
+    text: string,
+    html: string,
+    filePath: string,
+  ) {
+    const data: MailgunMessageData = {
+      from: 'Reaphsoft Workmen <noreply@reaphsoft-workmen.com>',
+      to: [recipient],
+      subject: subject,
+      text: text,
+      html: html,
+    };
+    try {
+      const fileData = fs.readFileSync(filePath);
+      data.attachment = {
+        data: fileData,
+      };
+    } catch (e) {
+      // todo log failure
+    }
+    return await this.sendMail(data);
+  }
+
+  async sendMail(data: MailgunMessageData) {
     let response: string = 'sent';
     await this.mg.messages
-      .create(this.domain, {
-        from: 'Reaphsoft Workmen <noreply@reaphsoft-workmen.com>',
-        to: [recipient],
-        subject: subject,
-        text: text,
-        html: html,
-      })
+      .create(this.domain, data)
       .then(() => (response = 'Queued'))
       .catch(() => {
         response = 'Error encountered';
       });
     return response;
   }
-
-  sendTextMailWithAttachment() {}
 }
