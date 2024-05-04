5810529b85317165712e61ff93b20ee3e455a79a
afolabi49 <108076216+McPherson49@users.noreply.github.com>
2024-05-04 07:58:52 +0100

    Estate House UI Add Modal

--- frontend/src/pages/Houses.js
+++ frontend/src/pages/Houses.js
@@ -1,14 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Modal } from "react-bootstrap";
 
 const Houses = ({user}) => {
+    const [estate, setEstate] = useState();
+    const [showModal, setShowModal] = useState(false);
+    const onhandleClick = (addEstate) => {
+        setEstate(addEstate);
+    }
+
+    const handleCloseModal = () => {
+        setShowModal(false);
+    };
     return (
         <div className="col-xl-9 col-lg-8 m-b30">
             <div className="job-bx browse-job clearfix">
                 <div className="job-bx-title  clearfix">
                     <h5 className="font-weight-700 pull-left text-uppercase text-black">Estate House</h5>
                     <div class="float-right">
-                        <span class="fa fa-plus text-black">Add Estate House</span>
+                        <span class="fa fa-plus text-black" onClick={() => {onhandleClick('addEstate'); setShowModal(true)}}>Add Estate House</span>
                     </div>
                 </div>
                 <table className="table-job-bx cv-manager company-manage-job">
@@ -34,8 +45,28 @@ const Houses = ({user}) => {
                         </tr>
                     </tbody>
                 </table>
-                
             </div>
+                <Modal class="modal-dialog" id={estate} role="document" show={showModal} >
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h4 class="title" id="defaultModalLabel">Add Estate Houses</h4>
+                        </div>
+                        <div class="modal-body col-sm-12">
+                            <div class="form-group">
+                                <label>House Owner</label>
+                                <input type="text" class="form-control" placeholder="Full Name" />
+                            </div>
+                            <div class="form-group">
+                                <label>House Number</label>
+                                <input type="Text" class="form-control" placeholder="Worker Email" />
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-default btn-round waves-effect">SAVE CHANGES</button>
+                            <button type="button" onClick={handleCloseModal} class="btn btn-danger waves-effect" data-dismiss="modal">CLOSE</button>
+                        </div>
+                    </div>
+                </Modal>
         </div>
     );
 }
