57f8eb08d43547b0a86c59c140c3b4f03c299d29
Felix Sihitshuwam <felixsigit@gmail.com>
2024-05-07 10:00:11 +0100

    Implemented change passwords

--- frontend/src/admin/pages/view-user.js
+++ frontend/src/admin/pages/view-user.js
@@ -143,18 +143,51 @@ export const ViewUser = () => {
     }
     const [disablePasswordButtons, setDisablePasswordButtons] = useState(false);
     const changePassword = (event) => {
-      event.preventDefault();
-      setDisablePasswordButtons(true);
-      if (passwordForm.password.length < 4 ){
-        setDisablePasswordButtons(false);
-        setPasswordErrorText('Password length should be more than 4');
-        return;
-      }
-      if (passwordForm.password !== passwordForm.password2){
-        setDisablePasswordButtons(false);
-        setPasswordErrorText('Passwords should be the same');
-        return;
-      }
+        event.preventDefault();
+        setDisablePasswordButtons(true);
+        if (passwordForm.password.length < 4 ){
+            setDisablePasswordButtons(false);
+            setPasswordErrorText('Password length should be more than 4');
+            return;
+        }
+        if (passwordForm.password !== passwordForm.password2){
+            setDisablePasswordButtons(false);
+            setPasswordErrorText('Passwords should be the same');
+            return;
+        }
+        setPasswordErrorText('');
+        fetch(`http://localhost:3001/admin/change/password/11/${email}/`, {
+            method: 'POST',
+            headers: {
+                'Authorization': 'Bearer ' + userAuth.admin.token,
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(passwordForm),
+        }).then( r => {
+            if (!r.ok){
+                showAlert(3, `Error while making request, please contact the system administrators. ${r.statusText}`, 'Error');
+                setPasswordForm({password2: '', password: ''});
+                setShowModal(false);
+                setDisablePasswordButtons(false);
+                return;
+            }
+            return r.json();
+        }).then(value => {
+            if (!value.status){
+                showAlert(3, value.resp, 'Error');
+                setDisablePasswordButtons(false);
+            }else {
+                showAlert(1, 'Password was changed successfully', 'Success');
+                setPasswordForm({password2: '', password: ''});
+                setShowModal(false);
+                setDisablePasswordButtons(false);
+            }
+        }).catch(reason => {
+            showAlert(3, reason.message, 'Error');
+            setPasswordForm({password2: '', password: ''});
+            setShowModal(false);
+            setDisablePasswordButtons(false);
+        });
     }
     const [passwordErrorText, setPasswordErrorText] = useState('')
     return (
