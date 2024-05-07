4765daa67d3f191dbf26bf042fc4168c182483be
Felix Sihitshuwam <felixsigit@gmail.com>
2024-05-07 09:12:42 +0100

    Added modal for changing passwords. Pre-posting.

--- frontend/src/admin/pages/view-user.js
+++ frontend/src/admin/pages/view-user.js
@@ -5,7 +5,7 @@
 import {useParams} from "react-router";
 import React, {useEffect, useState} from "react";
 import {useAuth} from "../../components/AuthContext";
-import {Button, Form, FormControl, Image, InputGroup} from "react-bootstrap";
+import {Button, Form, FormControl, FormGroup, FormLabel, Image, InputGroup, Modal} from "react-bootstrap";
 import {showAlert} from "../../utils/alert";
 import fp29332702_7495554 from '../components/fp29332702_7495554.jpg'
 
@@ -133,6 +133,30 @@ export const ViewUser = () => {
             setDisableSavePhoto(false);
         });
     }
+    const [showModal, setShowModal] = useState(false);
+    const [passwordForm, setPasswordForm] = useState({
+        password: '',
+        password2: '',
+    });
+    const handlePasswordChange = (e) => {
+      setPasswordForm({...passwordForm, [e.target.name]: e.target.value });
+    }
+    const [disablePasswordButtons, setDisablePasswordButtons] = useState(false);
+    const changePassword = (event) => {
+      event.preventDefault();
+      setDisablePasswordButtons(true);
+      if (passwordForm.password.length < 4 ){
+        setDisablePasswordButtons(false);
+        setPasswordErrorText('Password length should be more than 4');
+        return;
+      }
+      if (passwordForm.password !== passwordForm.password2){
+        setDisablePasswordButtons(false);
+        setPasswordErrorText('Passwords should be the same');
+        return;
+      }
+    }
+    const [passwordErrorText, setPasswordErrorText] = useState('')
     return (
       <section className="content">
           <div className="body_scroll">
@@ -190,7 +214,7 @@ export const ViewUser = () => {
                               <Form.Group className="col-10 offset-1 my-3">
                                   <Form.Label>Password</Form.Label>
                                   <InputGroup>
-                                      <Button variant="outline-primary">Edit</Button>
+                                      <Button onClick={() => setShowModal(true)} variant="outline-primary">Edit</Button>
                                       <Form.Control value="hashedpassword" className="my-1" readOnly={true}></Form.Control>
                                   </InputGroup>
                               </Form.Group>
@@ -231,6 +255,36 @@ export const ViewUser = () => {
                   </div>
               </div>
           </div>
+          <Modal
+              show={showModal}
+              backdrop="static"
+              keyboard={false}
+          >
+              <Modal.Header>
+                  <h5>Change Password</h5>
+              </Modal.Header>
+              <Modal.Body>
+                  <Form onSubmit={ changePassword }>
+                      <FormGroup className="mb-3 col-lg-8 offset-lg-2">
+                          <FormLabel>New Password</FormLabel>
+                          <FormControl required={true} type="password" autoComplete="new-password" name="password" value={passwordForm.password} onChange={ handlePasswordChange }></FormControl>
+                      </FormGroup>
+                      <div className="w-100"></div>
+                      <FormGroup className="mb-3 col-lg-8 offset-lg-2">
+                          <FormLabel>Repeat New Password</FormLabel>
+                          <FormControl required={true} name="password2" autoComplete="new-password" type="password" value={passwordForm.password2} onChange={ handlePasswordChange }></FormControl>
+                      </FormGroup>
+                      <div className="w-100"></div>
+                      <Form.Text className="text-danger text-center">{passwordErrorText}</Form.Text>
+                      <div className="text-center my-3">
+                          <Button variant="danger" type="submit" disabled={disablePasswordButtons}>Submit</Button>
+                      </div>
+                  </Form>
+              </Modal.Body>
+              <Modal.Footer>
+                  <Button disabled={disablePasswordButtons} onClick={() => {setShowModal(false)}}>Close</Button>
+              </Modal.Footer>
+          </Modal>
       </section>
     );
 }
