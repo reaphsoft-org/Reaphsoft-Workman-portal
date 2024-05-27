59c3648d743b99fb56577c21acc9ee67cdc017aa
Felix Sihitshuwam <felixsigit@gmail.com>
2024-05-27 16:05:55 +0100

    Changed frontend port to 80

--- backend/src/utilities/messenger.ts
+++ backend/src/utilities/messenger.ts
@@ -1,6 +1,7 @@
 // 24/05/2024 11:27
 // reaphsoft-workman
 // github.com/kahlflekzy
+/*
 
 import { applicationDefault, initializeApp, App } from 'firebase-admin/app';
 import { getMessaging } from 'firebase-admin/messaging';
@@ -32,4 +33,13 @@ export class Messenger {
             .then(() => {})
             .catch((e: any) => console.log(`Error sending message: ${e}`));
     }
+}
+*/
+
+export class Messenger {
+    async sendMessage(
+        registrationToken: string,
+        payload: { id: string },
+        messageContent: string,
+    ) {}
 }
