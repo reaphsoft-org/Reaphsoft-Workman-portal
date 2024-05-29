18813e069c7f7365e688dc0cb964e6585cde70fe
Felix Sihitshuwam <felixsigit@gmail.com>
2024-05-29 21:12:24 +0100

    Reverted some codes for the messenger utility.

--- backend/src/utilities/messenger.ts
+++ backend/src/utilities/messenger.ts
@@ -1,7 +1,6 @@
 // 24/05/2024 11:27
 // reaphsoft-workman
 // github.com/kahlflekzy
-/*
 
 import { applicationDefault, initializeApp, App } from 'firebase-admin/app';
 import { getMessaging } from 'firebase-admin/messaging';
@@ -34,12 +33,11 @@ export class Messenger {
             .catch((e: any) => console.log(`Error sending message: ${e}`));
     }
 }
-*/
 
-export class Messenger {
+/*export class Messenger {
     async sendMessage(
         registrationToken: string,
         payload: { id: string },
         messageContent: string,
     ) {}
-}
+}*/
